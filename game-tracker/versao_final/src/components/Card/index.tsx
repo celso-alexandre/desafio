0c10424c19f7733947f9c48bd5ec8692462a2b5e
celso-alexandre <celsoalexandre@live.com>
2021-03-03 17:11:01 +0000

    fix: Added better thumbnails

--- game-tracker/versao_final/src/components/Card/index.tsx
+++ game-tracker/versao_final/src/components/Card/index.tsx
@@ -1,62 +1,62 @@
 import React from 'react';
 
 import {
-  Container, DetailsButton, OriginalPrice, DisccountPercentage, CardFooter,
+   Container, DetailsButton, OriginalPrice, DisccountPercentage, CardFooter,
 } from './styles';
 
 interface ICardProps {
-  title: string;
-  image: any;
-  originalPrice?: number;
-  actualPrice: number
+   title: string;
+   image: any;
+   originalPrice?: number;
+   actualPrice: number
 }
 
 const Card: React.FC<ICardProps> = ({
-  title,
-  image,
-  actualPrice,
-  originalPrice = actualPrice,
+   title,
+   image,
+   actualPrice,
+   originalPrice = actualPrice,
 }: ICardProps) => {
-  return (
-    <Container>
-      {image}
-      <h2>{title}</h2>
+   return (
+      <Container>
+         {image}
+         <h2>{title}</h2>
 
-      <CardFooter>
-        <div>
-          <DetailsButton>DETALHES</DetailsButton>
-        </div>
+         <CardFooter>
+            <div>
+               <DetailsButton>DETALHES</DetailsButton>
+            </div>
 
-        <div>
-          <div>
-            {originalPrice && originalPrice !== actualPrice && (
-            <>
-              <OriginalPrice>
-                $
+            <div>
+               <div>
+                  {originalPrice && originalPrice !== actualPrice && (
+                     <>
+                        <OriginalPrice>
+                           $
                 {' '}
-                {originalPrice.toFixed(2).replace('.', ',')}
-              </OriginalPrice>
-              <br />
-            </>
-            )}
-            <span>
-              $
+                           {originalPrice.toFixed(2).replace('.', ',')}
+                        </OriginalPrice>
+                        <br />
+                     </>
+                  )}
+                  <span>
+                     $
               {' '}
-              {actualPrice.toFixed(2).replace('.', ',')}
-            </span>
-          </div>
+                     {actualPrice.toFixed(2).replace('.', ',')}
+                  </span>
+               </div>
 
-          <div>
-            <DisccountPercentage>
-              {actualPrice === 0 ? 'GRÁTIS' : `${(
-                ((originalPrice - actualPrice) / originalPrice) * 100
-              ).toFixed(2).replace('.', ',')} %`}
-            </DisccountPercentage>
-          </div>
-        </div>
-      </CardFooter>
-    </Container>
-  );
+               <div>
+                  <DisccountPercentage>
+                     {actualPrice === 0 ? 'GRÁTIS' : `${(
+                        ((originalPrice - actualPrice) / originalPrice) * 100
+                     ).toFixed(0).replace('.', ',')} %`}
+                  </DisccountPercentage>
+               </div>
+            </div>
+         </CardFooter>
+      </Container>
+   );
 };
 
 export default Card;
--- game-tracker/versao_final/src/components/Main/index.tsx
+++ game-tracker/versao_final/src/components/Main/index.tsx
@@ -2,44 +2,59 @@ import React, { useEffect, useState } from 'react';
 import Card from '../Card';
 
 import { Container, CardContainer } from './styles';
-import ImgRage2 from '../../assets/rage2.jpg';
 import api from '../../utils/api';
 
 interface IGame {
-  internalName: string;
-  title: string;
-  gameID: string;
-  normalPrice: string;
-  salePrice: string;
-  isOnSale: '0' | '1',
-  thumb: string,
-  dealRating: string,
+   internalName: string;
+   title: string;
+   gameID: string;
+   normalPrice: string;
+   salePrice: string;
+   isOnSale: '0' | '1';
+   dealRating: string;
+   steamAppID: string;
+   thumb: string;
+   betterThumb?: string;
 }
 
 const Main: React.FC = () => {
-  const [games, setGames] = useState<IGame[]>([]);
+   const [games, setGames] = useState<IGame[]>([]);
 
-  useEffect(() => {
-    api.get('https://www.cheapshark.com/api/1.0/deals?pageNumber=0&pageSize=99&storeID=1&onSale=1&AAA=1')
-      .then((response) => { return setGames(response?.data); });
-  }, []);
+   useEffect(() => {
+      let gamesResponse = [] as unknown as IGame[];
+      api.get('https://www.cheapshark.com/api/1.0/deals?pageNumber=0&pageSize=15&storeID=1&onSale=1&AAA=1')
+         .then((response) => {
+            gamesResponse = response?.data;
 
-  return (
-    <Container>
-      <CardContainer>
-        {games?.map((game_) => {
-          return (
-            <Card
-              image={(<img src={game_?.thumb} alt="thumbnail" />)}
-              title={game_?.title}
-              originalPrice={parseFloat(game_?.normalPrice)}
-              actualPrice={parseFloat(game_?.salePrice)}
-            />
-          );
-        })}
-      </CardContainer>
-    </Container>
-  );
+            if (gamesResponse) {
+               const updatedGames = gamesResponse.map((gameResponse) => {
+                  return {
+                     betterThumb: `https://cdn.akamai.steamstatic.com/steam/apps/${gameResponse?.steamAppID}/header.jpg`,
+                     ...gameResponse,
+                  };
+               });
+
+               setGames(updatedGames);
+            }
+         });
+   }, []);
+
+   return (
+      <Container>
+         <CardContainer>
+            {games?.map((game_) => {
+               return (
+                  <Card
+                     image={(<img src={game_?.betterThumb} alt="thumbnail" />)}
+                     title={game_?.title}
+                     originalPrice={parseFloat(game_?.normalPrice)}
+                     actualPrice={parseFloat(game_?.salePrice)}
+                  />
+               );
+            })}
+         </CardContainer>
+      </Container>
+   );
 };
 
 export default Main;
